e2f541ec299f6c19db12555975b74756d8c7e181
umang70 <153925119+umang70@users.noreply.github.com>
2024-05-19 17:30:15 +0530

    first commit

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-const btn=document.getElementById("button")
-const input=document.getElementById("city-input")
-const cityName=document.getElementById("city-name")
-const cityTemp=document.getElementById("city-temp")
-const cityTime=document.getElementById("city-time")
- async function getData(cityname){
-      const promise=await fetch(`https://api.weatherapi.com/v1/current.json?key=dcfef66a77664348886144007240503&q=${cityname}&aqi=yes`)
-      return await promise.json()
+const button=document.getElementById("get-location-button");
+function gotlocation(position){
+    console.log(position)
+
+}
+function failed(){
+    console.log("Failed to get location");
 
 }
 
 
+button.addEventListener("click", async function(){
+    const location=navigator.geolocation.getCurrentPosition(gotlocation ,failed );
 
 
-
-btn.addEventListener("click",  async function(){
-   const value=input.value
-  const result=await getData(value)
-  cityName.innerText=`${result.location.name}`
-  cityTemp.innerText=`${result.current.temp_c}`
-  cityTime.innerText=`${result.location.localtime}`
-  
 })
-//https://api.weatherapi.com/v1/current.json?key=dcfef66a77664348886144007240503&q=London&aqi=yes
